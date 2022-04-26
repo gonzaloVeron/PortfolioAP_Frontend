adec22af36c9091c58ccbf3bb17b0d19996d79ae
GonzaloVeron <gonveron96@gmail.com>
2022-04-26 12:37:59 -0300

    Feat(AboutMe, Experiences, Main, NavBar, UserAvatar) agrego componentes al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "app",
+    pathMatch: "full"
+  },
+  {
+    path: "app",
+    component: ExperiencesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [	
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    UserAvatarComponent,
+    AboutMeComponent,
+    ExperiencesComponent
   ],
   imports: [
     BrowserModule,
